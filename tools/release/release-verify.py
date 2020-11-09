d60a653740bf460f269c2f51ed1f4b7e3fb9fea5
Sarena Meas <sarem@amazon.com>
2020-11-09 14:07:43 -0800

    Check validate branches in CSDK and library repos.

--- tools/release/release-verify.py
+++ tools/release/release-verify.py
@@ -4,7 +4,6 @@ import sys
 import argparse
 import os
 import zipfile
-from github import Github
 import yaml
 import requests
 from requests.auth import HTTPBasicAuth
@@ -18,15 +17,16 @@ CSDK_BRANCHES = ["master", "v4_beta_deprecated"]
 
 CSDK_ORG = "sarenameas"
 CSDK_REPO = "aws-iot-device-sdk-embedded-c"
+
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_ACCESS_TOKEN = ""
 GITHUB_AUTH_HEADER = {"Authorization": "token %s" % GITHUB_ACCESS_TOKEN, "Accept": "application/vnd.github.v3+json"}
+
 JENKINS_API_URL = "https://amazon-freertos-ci.corp.amazon.com/"
 JENKINS_USERNAME = ""
 JENKINS_PASSWORD = ""
-JENKINS_CSDK_DEMOS_PATH = "/job/csdk/job/demo_pipeline/lastBuild/api/json"
-JENKINS_CSDK_TESTS_PATH = "/job/csdk/job/nightly/lastBuild/api/json"
-JENKINS_AUTH_HEADER = {"Authorization": "Basic %s:%s" % (JENKINS_USERNAME, JENKINS_PASSWORD)}
+JENKINS_CSDK_DEMOS_PATH = "/job/csdk/job/demo_pipeline/lastCompletedBuild/api/json"
+JENKINS_CSDK_TESTS_PATH = "/job/csdk/job/nightly/lastCompletedBuild/api/json"
 
 
 def run_cmd(cmd):
@@ -53,17 +53,15 @@ def run_cmd(cmd):
 # TODO: Create rollback function.
 
 
-def validate_manifest(csdk_root, csdk_version, lib_versions):
+def validate_manifest(manifest, csdk_version, lib_versions):
     """
     Validates the manifest.yml file at the root of the CSDK.
     Args:
-        csdk_root (str): The root of the CSDK repo.
+        manifest (dict): The CSDK's manifest.yml loaded into a dictionary.
         csdk_versions (str): The new version of the CSDK repo.
         lib_versions (dict): A dictionary containing the new versions of each library.
         Please see tools/release/config.yml.
     """
-    with open(os.path.join(csdk_root, "manifest.yml")) as manifest_file:
-        manifest = yaml.load(manifest_file, Loader=yaml.FullLoader)
 
     manifest_version = manifest["version"]
     if manifest_version != csdk_version:
@@ -84,12 +82,69 @@ def validate_manifest(csdk_root, csdk_version, lib_versions):
     print("PASS: manifest.yml contains all libraries and their versions.")
 
 
+def validate_checks(repo_paths):
+    """
+    Validates that all of the GHA and CBMC status checks passed on all repos.
+    Args:
+        repo_paths (dict): Paths to all library repos in the CSDK, including their org.
+    """
+    for repo_path in repo_paths:
+        git_req = requests.get(f"{GITHUB_API_URL}/{repo_path}/commits/master/check-runs", headers=GITHUB_AUTH_HEADER)
+        # The first item is the latest commit on master.
+        if git_req.json()["check_runs"][0]["conclusion"] != "success":
+            raise Exception(f"The GHA status checks failed for {repo}.")
+        git_req = requests.get(f"{GITHUB_API_URL}/{repo_path}/commits/master/status", headers=GITHUB_AUTH_HEADER)
+        if git_req.json()["state"] != "success":
+            raise Exception(f"The CBMC status checks failed for {repo}.")
+
+
+def validate_ci():
+    """
+    Validates that all CSDK jobs in the Jenkins CI passed.
+    """
+    jenkins_req = requests.get(
+        f"{JENKINS_API_URL}/{JENKINS_CSDK_DEMOS_PATH}", auth=HTTPBasicAuth(JENKINS_USERNAME, JENKINS_PASSWORD)
+    )
+    if jenkins_req.json()["result"] != "SUCCESS":
+        raise Exception(f"Jenkins job {JENKINS_API_URL}/{JENKINS_CSDK_DEMOS_PATH} failed.")
+    jenkins_req = requests.get(
+        f"{JENKINS_API_URL}/{JENKINS_CSDK_TESTS_PATH}", auth=HTTPBasicAuth(JENKINS_USERNAME, JENKINS_PASSWORD)
+    )
+    if jenkins_req.json()["result"] != "SUCCESS":
+        raise Exception(f"Jenkins job {JENKINS_API_URL}/{JENKINS_CSDK_TESTS_PATH} failed.")
+
+
+def validate_branches(repo_paths):
+    """
+    Validates that only the master branch exists on each library repo.
+    Args:
+        repo_paths (dict): Paths to all library repos in the CSDK, including their org.
+    """
+    for repo_path in repo_paths:
+        git_req = requests.get(f"{GITHUB_API_URL}/{repo_path}/branches/", headers=GITHUB_AUTH_HEADER)
+        valid_branches = ["master"]
+        if repo_path == "{CSDK_ORG}/{CSDK_REPO}":
+            valid_branches.append("v4_beta_deprecated")
+        if len(git_req) != len(valid_branches):
+            raise Exception(f"Invalid number of branches {len(git_req)} in {repo_path}.")
+        for branch in git_req.json():
+            branch_name = branch["name"]
+            if branch_name not in valid_branches:
+                raise Exception(f"Invalid branch {branch_name} found in {repo_path}.")
+
+
+def validate_release_candidate_branches():
+    """
+    Verifies there are no pending PRs to the release candidate branch.
+    """
+    git_req = requests.get(f"{GITHUB_API_URL}/{CSDK_ORG}/{CSDK_REPO}/pulls?base=release_candidate", headers=GITHUB_AUTH_HEADER)
+    if(len(git_req.json()) > 0):
+        None
+
 def main():
     """
-    Update the CSDK and its library repos for release.
+    Performs pre-release validation of the CSDK and the library spoke repos.
     """
-
-    # Options:
     parser = argparse.ArgumentParser(description="Perform CSDK Release activities.")
     parser.add_argument("-r", "--root", action="store", required=True, dest="root", help="CSDK repo root path.")
 
@@ -100,12 +155,15 @@ def main():
     with open(os.path.join(csdk_root, "tools", "release", "config.yml")) as config_file:
         configs = yaml.load(config_file, Loader=yaml.FullLoader)
 
-    # Verify that Manifest.yml has all libraries and their versions.
-    validate_manifest(csdk_root, configs["csdk_version"], configs["versions"])
+    # Parse the manifest.yml
+    with open(os.path.join(csdk_root, "manifest.yml")) as manifest_file:
+        manifest = yaml.load(manifest_file, Loader=yaml.FullLoader)
+    repo_paths = []
+    for dep in manifest["dependencies"]:
+        repo_paths.append(dep["url"][dep["url"].find(".com/") + len(".com/")])
+    repo_paths.append("{CSDK_ORG}/{CSDK_REPO}")
 
-    # Check that release-candidate is created. I think it's a bit much for the
-    # script to create release-candidate for you. I think that should be part of
-    # the CM, so that it is more intentional.
+    # Get the authentication variables
     access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
     if access_token == None:
         access_token = configs["github_access_token"]
@@ -115,44 +173,20 @@ def main():
     JENKINS_USERNAME = configs["jenkins_username"]
     JENKINS_PASSWORD = configs["jenkins_password"]
 
-    git = Github(access_token)
-    git_repo = git.get_organization(CSDK_ORG).get_repo(CSDK_REPO)
+    # Verify that Manifest.yml has all libraries and their versions.
+    validate_manifest(manifest, configs["csdk_version"], configs["versions"])
 
-    # Update the library submodules to the latest. A PR will need to be created for this.
-    # TODO: Consider taking this out. This might need to be a CM step so it is
-    # more intentional.
-    run_cmd("git submodule update --remote libraries/aws/* libraries/standard/*")
+    # Verify status checks in all repos.
+    validate_checks(repo_paths)
 
-    # Check that all GHA actions pass in all libraries.
-    # Github actions support is not officially released yet in pygithub. We must
-    # use the REST API.
-    # https://api.github.com/repos/freertos/corehttp/actions/runs?branch=master Checks all GHA runs on the master branch.
-    # https://api.github.com/repos/freertos/corehttp/commits/master/check-runs Checks ALL checks GHA
-    # https://api.github.com/repos/freertos/corehttp/commits/master/status Checks the status of CBMA and other non GHA checks
-    for repo in config["versions"].keys():
-        gha_req = requests.get(f"{GITHUB_API_URL}/{repo}/commits/master/check-runs", headers=GITHUB_AUTH_HEADER)
-        # The first item is the latest commit on master.
-        if gha_req.json()["check_runs"][0]["conclusion"] != "success":
-            raise Exception(f"The GHA status checks failed for {repo}.")
-        gha_req = requests.get(f"{GITHUB_API_URL}/{repo}/commits/master/status", headers=GITHUB_AUTH_HEADER)
-        if gha_req.json()["state"] != "success":
-            raise Exception(f"The CBMC status checks failed for {repo}.")
+    # Validate that the jenkins CI passed.
+    validate_ci()
 
     # Check a repo that only qualified branches exist
-    jenkins_req = requests.get(f"{JENKINS_API_URL}/{JENKINS_CSDK_DEMOS_PATH}", auth=HTTPBasicAuth(JENKINS_USERNAME, JENKINS_PASSWORD))
-    if jenkins_req.json()["result"] != "SUCCESS":
-        raise Exception(f"Jenkins job {JENKINS_API_URL}/{JENKINS_CSDK_DEMOS_PATH} failed.")
-    jenkins_req = requests.get(f"{JENKINS_API_URL}/{JENKINS_CSDK_TESTS_PATH}", auth=HTTPBasicAuth(JENKINS_USERNAME, JENKINS_PASSWORD))
-    if jenkins_req.json()["result"] != "SUCCESS":
-        raise Exception(f"Jenkins job {JENKINS_API_URL}/{JENKINS_CSDK_TESTS_PATH} failed.")
+    validate_branches(repo_paths)
 
-    
-
-    # Tag each of the library repos.
-
-    # Verify submodules point to each of the new tags.
-
-    None
+    # Verify there are no pending PRs to the release-candidate branch.
+    validate_release_candidate_branches()
 
 
 if __name__ == "__main__":
